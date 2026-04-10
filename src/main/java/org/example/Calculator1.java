127dbdfb5c64c15028eabc6cd7e8a979d0726a5a
MichaelTS2001 <michaeltongasani2001@gmail.com>
2026-04-10 11:16:28 -0400

    Initial commit

--- src/main/java/org/example/Calculator1.java
+++ src/main/java/org/example/Calculator1.java
@@ -1,8 +1,41 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator1 {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Loan Amount:");
+
+        double principle = input.nextDouble();
+
+        System.out.println("Interest rate:");
+
+        double INTEREST_RATE = input.nextDouble();
+
+        System.out.println("Loan term:");
+
+        int loanLength = input.nextInt();
+
+        double monthlyInterestRate = INTEREST_RATE / 12 / 100; // the interest rate for each month
+
+        int montlyPaymentAmount = 12 * loanLength; // number of monthly payments for however long the loan is
+
+        double monthlyPayment = principle * ( monthlyInterestRate * Math.pow(1 + monthlyInterestRate, montlyPaymentAmount )
+                                / ( ( Math.pow( 1 + monthlyInterestRate, montlyPaymentAmount ) ) -1 ) );
+
+        double total_Interest = (monthlyPayment * montlyPaymentAmount) - principle;
+
+        System.out.println();
+
+        System.out.println("Expected Monthly Payment: $" + monthlyPayment);
+        System.out.println("Total Interest: $" + total_Interest);
+
+
+
 
     }
 }
--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -1,8 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator2 {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Deposit amount:");
+
+        double deposit = input.nextDouble();
+
+        System.out.println("Annual Percentage Yield (APY):");
+
+        double ANNUAL_INTEREST_RATE = input.nextDouble();
+
+        System.out.println("CD Term Length:");
+
+        double interest_rate = ANNUAL_INTEREST_RATE / 100;
+
+        int termLength = input.nextInt();
+
+        int numDays = 365 * termLength;
+
+        double futureValue = deposit * ( Math.pow( 1 + ( interest_rate / 365 ), 365 * termLength)));
 
     }
 }
